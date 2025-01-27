0a97162784235558db1375c2ab17acc11f14fd0b
PdeAndres <pablodeandresolano@gmail.com>
2025-01-27 20:45:12 +0100

    Ej directivas

--- directivas/src/app/app.component.ts
+++ directivas/src/app/app.component.ts
@@ -1,13 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+export interface Persona {
+  nombre: string;
+  apellidos: string;
+  edad: number;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'directivas';
+  titulo: string = 'Alta de usuario';
+  nombre: string = '';
+  apellidos: string = '';
+  enviado: boolean = false;
+  morado: string = 'morado';
+
+  listaPersonas: Persona[] = [
+    {
+      nombre: 'Pablo',
+      apellidos: 'de Andrés',
+      edad: 30,
+    },
+    {
+      nombre: 'Manuela',
+      apellidos: 'Gonzalez',
+      edad: 26,
+    },
+  ];
+  registrar() {
+    this.enviado = true;
+  }
 }
